95521cc466f03d5b4fb7fd11ce78536d6270da56
Jason Mossing <c15jason.mossing@usafa.edu>
2013-10-24 13:51:15 -0600

    B functionality

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 
 #include <msp430.h> 
 #include "LCD.h"
+#include "button.h"
+
 /*
  * main.c
  */
@@ -16,11 +18,33 @@ int main(void) {
     initSPI();
     initLCD();
     clearLCD();
-
+    configureP2PinAsButton(BIT3|BIT4|BIT5);
     char* string1 = "ECE382 is my favorite class! ";
-    char* string2 = "Here is line2! ";
+    char* string2;
+
+    writeString("Message?");
+    line2Cursor();
+    writeString("PRESS123");
+
+    char buttons[] = {BIT3, BIT4, BIT5};
+    char pressedButton = pollP2Buttons(buttons, 3);
+
+    if(pressedButton == BIT3){
+    	string2 = "You pressed button 1. ";
+    	waitForP2ButtonRelease(BIT3);
+    }
+    else if(pressedButton == BIT4){
+    	string2 = "You pressed button 2. ";
+    	waitForP2ButtonRelease(BIT4);
+    }
+    else{
+    	string2 = "You pressed button 3. ";
+    	waitForP2ButtonRelease(BIT5);
+    }
+
     scrollString(string1, string2);
 
+    //traps cpu
     while(1){
 
     }
